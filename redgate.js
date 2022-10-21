a4ee07b0cddc59cf839722818433963d02d667f3
Jon Kirkwood <re_kirky@hotmail.com>
2022-10-21 11:59:10 +1000

    updated js
    include date lookup

--- redgate.js
+++ redgate.js
@@ -14,3 +14,20 @@ function myFunction() {
 		}
 	}
 }
+
+function myFilter() {
+	var input, filter, ul, li, a, i, txtValue;
+	input = document.getElementById('myYear');
+	filter = input.value.toUpperCase();
+	ul = document.getElementById('myUL');
+	li = ul.getElementsByTagName('li');
+	for (i = 0; i < li.length; i++) {
+		a = li[i].getElementsByTagName('span')[0];
+		txtValue = a.textContent || a.innerText;
+		if (txtValue.toUpperCase().indexOf(filter) > -1) {
+			li[i].style.display = '';
+		} else {
+			li[i].style.display = 'none';
+		}
+	}
+}
