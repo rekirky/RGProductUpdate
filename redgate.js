6691b012ba2e5f5ab1129a33da62d380ba703b34
GitHub Action <action@github.com>
2022-10-21 09:44:26 +0000

    update data

--- redgate.js
+++ redgate.js
@@ -21,12 +21,12 @@ function myFilter() {
 	ul = document.getElementById('myUL');
 	li = ul.getElementsByTagName('li');
 	for (i = 0; i < li.length; i++) {
-	a = li[i].getElementsByTagName('span')[0];
+		a = li[i].getElementsByTagName('span')[0];
 		txtValue = a.textContent || a.innerText;
 		if (txtValue.toUpperCase().indexOf(filter) > -1) {
 			li[i].style.display = '';
 		} else {
 			li[i].style.display = 'none';
-			}
+		}
 	}
 }
