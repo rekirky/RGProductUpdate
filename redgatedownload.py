b45c4f4b25f252e7de176323da598abb5dd4db22
Jon Kirkwood <re_kirky@hotmail.com>
2022-11-02 13:14:48 +1000

    Added patch notes
    Initial rollout of patch notes
    Need to further flesh out links in releasenotes.csv for all products.

--- redgatedownload.py
+++ redgatedownload.py
@@ -3,6 +3,11 @@ import xmltodict
 import urllib.request
 import re
 from datetime import datetime, date
+import csv
+from csv import DictReader
+from bs4 import BeautifulSoup
+
+# Files in project
 
 #to-do 
 #add functionality to check for changes since last update / last week
@@ -58,13 +63,19 @@ def get_updates(products):
     return(prod_list)
     
 def create_html(prod_list):
+    tooltip = ""
     file_out = open(f"index.html","w")
     file_out.write("<head>\n<title>Redgate Product Download Links</title>\n<link rel='stylesheet' href='https://cdn.rd.gt/assets/styles/isw.css?v=1637587319771'>\n<link rel='stylesheet' href='redgate.css'>\n</head>\n")
     file_out.write("<body>\n<script src='redgate.js'></script>\n")
-    file_out.write(f"<h1>Page updated: {date.today().strftime('%Y/%m/%d')} | {datetime.now().strftime('%H:%M:%S')}</h1>\n")
-    file_out.write(f"<input type='text' id='myInput' onkeyup='myFunction()' placeholder='Search for product..'>")
-    file_out.write(f"<input type='text' id='myYear' onkeyup='myFilter()' placeholder='Search for updated date..'>\n")
-    file_out.write("<ul id = 'myUL'>\n")
+    file_out.write(f"<h1>Redgate Product Links</h1>\n\t")
+    file_out.write(f"<div>Links to the most recent version of Redgate products allowing searching by product & release date</div>\n\t")
+    file_out.write(f"<div>Products updated this year are Green, last year Orange & previous years Red</div>\n\t")
+    file_out.write(f"<div>Tooltips may be available when hovering over products updated this year</div>\n\t")
+    file_out.write(f"This is a passion project and is an ongoing work in progress</div>\n")
+    file_out.write(f"<h2>Page updated: {date.today().strftime('%Y/%m/%d')} | {datetime.now().strftime('%H:%M:%S')}</h2>\n\n")
+    file_out.write(f"<input type='text' id='myInput' onkeyup='myFunction()' placeholder='Search by product..'>\n")
+    file_out.write(f"<input type='text' id='myYear' onkeyup='myFilter()' placeholder='Search by updated date..'>\n")
+    file_out.write("<ul id = 'myUL'>\n\t")
     for i in prod_list:
         for x in i:
             xproduct = x['product']
@@ -81,9 +92,10 @@ def create_html(prod_list):
                 xclass = 'old'
             try:
                 ver = re.search("[0-9]*\.[0-9]*\.[0-9]*",xlink)
-                file_out.write(f"<li class={xclass}><a href={xlink}><b>{xproduct} - {ver.group()}</b></a><span> - Updated {xdate}</span></li>\n")
+                tooltip = patch_notes(xproduct)
+                file_out.write(f"<li title='{tooltip}' class={xclass}>\n\t\t<a href={xlink}><b>{xproduct} - {ver.group()}</b></a><span> - Updated {xdate}</span></li>\n\t")
             except:
-                file_out.write(f"<li class={xclass}><a href={xlink}><b>{xproduct}</b></a><span> - Updated {xdate}</span></li>\n")
+                file_out.write(f"<li class={xclass}>\n\t\t<a href={xlink}><b>{xproduct}</b></a><span> - Updated {xdate}</span></li>\n\t")
     file_out.write(f"</ul>\n</body>\n")
     file_out.close()    
 
@@ -94,8 +106,8 @@ def create_css(): #write the css file
     file_out.write("ul .current {\n\tcolor:green;\n}\n")
     file_out.write("ul .previous {\n\tcolor:orange;\n}\n")
     file_out.write("ul .old {\n\tcolor:red;\n}\n")
-    file_out.write("#myInput {\n\twidth: 25%;\n}")
-    file_out.write("#myYear {\n\twidth: 25%;\n}")
+    file_out.write("#myInput {\n\twidth: 25%;\n}\n")
+    file_out.write("#myYear {\n\twidth: 25%;\n}\n")
     file_out.close()
 
 def create_js(): #write the js file for searching
@@ -104,8 +116,42 @@ def create_js(): #write the js file for searching
     file_out.write("filter = input.value.toUpperCase();\n\tul = document.getElementById('myUL');\n\tli = ul.getElementsByTagName('li');\n\t")
     file_out.write("for (i = 0; i < li.length; i++) {\n\t\ta = li[i].getElementsByTagName('a')[0];\n\t\ttxtValue = a.textContent || a.innerText;\n\t\t")
     file_out.write("if (txtValue.toUpperCase().indexOf(filter) > -1) {\n\t\t\tli[i].style.display = '';\n\t\t} else {\n\t\t\t")
-    file_out.write("li[i].style.display = 'none';\n\t\t}\n\t}\n}")
+    file_out.write("li[i].style.display = 'none';\n\t\t}\n\t}\n}\n")
+    
+    file_out.write("function myFilter() {\n\tvar input, filter, ul, li, a, i, txtValue;\n\tinput = document.getElementById('myYear');\n\t")
+    file_out.write("filter = input.value.toUpperCase();\n\tul = document.getElementById('myUL');\n\tli = ul.getElementsByTagName('li');\n\t")
+    file_out.write("for (i = 0; i < li.length; i++) {\n\t\ta = li[i].getElementsByTagName('span')[0];\n\t\ttxtValue = a.textContent || a.innerText;\n\t\t")
+    file_out.write("if (txtValue.toUpperCase().indexOf(filter) > -1) {\n\t\t\tli[i].style.display = '';\n\t\t} else {\n\t\t\t")
+    file_out.write("li[i].style.display = 'none';\n\t\t}\n\t}\n}\n")
     file_out.close
+
+def patch_notes(product_list):
+    tool_tip = []
+    with open('releasenotes.csv','r') as data:
+        reader = csv.reader(data)
+        url_dict = dict((rows[0], rows[1]) for rows in reader)
+        data.close()
+        link = f"{url_dict[product_list]}"
+        url = requests.get(link)
+        soup = BeautifulSoup(url.text,features="html.parser")
+        for items in soup.find_all("h2",limit=1):
+            for item in items.find_next_siblings():
+                if item.name != 'h2':
+                    for li in item:
+                        tool_tip.append(li.get_text(strip=True))
+                else:
+                    break
+    return(tool_tip_html(tool_tip))
+
+
+def tool_tip_html(tool_tip):
+    html = ""
+    for i in tool_tip:
+        html += i.replace('\n',' ')
+        html += '&#013'
+    return(html)
+
+
 
 # run program
 if __name__ == '__main__':
